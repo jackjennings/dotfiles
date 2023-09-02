d84596923c93d80f82099c004c67ef39ca40a215
Jack Jennings <jack.jennings@leafly.com>
2023-09-01 18:04:00 -0700

    Add defaults script

---
+++ home/run_onchange_macos-defaults.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+# Dock
+
+## Hide dock by default
+defaults write com.apple.dock "autohide" -bool "true"
+
+## Hide recent files
+defaults write com.apple.dock "show-recents" -bool "false"
+
+## Only show active applications in dock
+defaults write com.apple.dock "static-only" -bool "true"
+
+killall Dock
+
+# Safari
+
+## Show full URL 
+defaults write com.apple.Safari "ShowFullURLInSmartSearchField" -bool "true" && killall Safari
+
+# Finder
+
+## Show all file extensions
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
+
+## Disable file extension change warning
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+
+killall Finder
+
+# Menu bar
+
+## Set time format
+
+defaults write com.apple.menuextra.clock "DateFormat" -string "\"MMM d HH:mm\""
+
+# TextEdit
+
+## Use plain text by default
+defaults write com.apple.TextEdit "RichText" -bool "false"
+
