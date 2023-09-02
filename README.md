01752f96909aa636a7107db6d21046e4f1e65650
Jack Jennings <jack.jennings@leafly.com>
2023-09-01 17:12:03 -0700

    Add bootstapping script

---
+++ README.md
@@ -0,0 +1,7 @@
+# dotfiles
+
+## Bootstrapping
+
+```sh
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/jackjennings/dotfiles/HEAD/bootstrap.sh)"
+```
---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+if ! type "brew" > /dev/null; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+/opt/homebrew/bin/brew install chezmoi gh
+
+if [ ! -f ~/.ssh/id_rsa ]; then
+  ssh-keygen -f ~/.ssh/id_rsa
+fi
+
+/opt/homebrew/bin/gh auth login \
+  --git-protocol ssh \
+  --hostname github.com
+
+/opt/homebrew/bin/gh auth setup-git
+
+/opt/homebrew/bin/chezmoi init --apply https://github.com/$GITHUB_USERNAME/dotfiles.git
