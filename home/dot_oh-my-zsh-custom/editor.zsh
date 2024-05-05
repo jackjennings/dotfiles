202163287e4e347f80c85fc9a53111d99344af98
Jack Jennings <jack.jennings@leafly.com>
2024-05-05 12:31:44 -0700

    Update editor aliased

--- home/dot_oh-my-zsh-custom/editor.zsh
+++ home/dot_oh-my-zsh-custom/editor.zsh
@@ -2,4 +2,8 @@ EDITOR="vim"
 
 alias e="$EDITOR ."
 alias c="code ."
-alias zshconfig="$EDITOR ~/.zshrc"
+alias ezr="$EDITOR ~/.zshrc"
+alias ezo="$EDITOR ~/.oh-my-zsh"
+alias ezc="$EDITOR ~/.oh-my-zsh-custom"
+alias cch="code --add . $(git ls-files -m)"
+
