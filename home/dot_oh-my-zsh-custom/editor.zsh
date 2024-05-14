39ec24cb362059e3c7bb3da8deab7509ab264f20
Jack Jennings <j@ckjennin.gs>
2024-05-14 07:53:18 -0700

    Fix cch alias from checking git files on source

--- home/dot_oh-my-zsh-custom/editor.zsh
+++ home/dot_oh-my-zsh-custom/editor.zsh
@@ -5,5 +5,8 @@ alias c="code ."
 alias ezr="$EDITOR ~/.zshrc"
 alias ezo="$EDITOR ~/.oh-my-zsh"
 alias ezc="$EDITOR ~/.oh-my-zsh-custom"
-alias cch="code --add . $(git ls-files -m)"
+
+function cch () {
+	code --add . $(git ls-files -m);
+}
 
