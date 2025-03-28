56eab4859c0a24e26ad5c5d5591c13dd1b2425c1
Jack Jennings <j@ckjennin.gs>
2025-03-28 14:29:34 -0700

    Add asdf plugins script

---
+++ home/run_onchange_asdf_plugins.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+asdf plugin add deno https://github.com/asdf-community/asdf-deno.git
+asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
+asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+asdf plugin add python
+asdf plugin add ruby https://github.com/asdf-vm/asdf-ruby.git
