5ed4d786b515265a0cd4ffbd49c62a0dde62ff48
Jack Jennings <j@ckjennin.gs>
2025-08-25 10:03:03 -0700

    Fix issues with cold start

---
+++ home/run_onchange_launch_applets.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+osascript -e 'tell application "Hammerspoon" to activate'
+osascript -e 'tell application "Caffeine" to activate'
+osascript -e 'tell application "Bartender 5" to activate'
+osascript -e 'tell application "ProtonVPN" to activate'
+
