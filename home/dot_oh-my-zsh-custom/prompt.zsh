ecc7aa31000a1691264d25df817364dfdc15797b
Jack Jennings <j@ckjennin.gs>
2026-06-29 09:24:43 -0700

    prompt: show (wt) in RPROMPT when inside a git worktree

--- home/dot_oh-my-zsh-custom/prompt.zsh
+++ home/dot_oh-my-zsh-custom/prompt.zsh
@@ -1,5 +1,9 @@
 setopt prompt_subst
 
+_git_worktree_indicator() {
+  [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] && echo ' (wt)'
+}
+
 PROMPT='%B%F{7}%1~%f%b ⎬ '
-RPROMPT='%B%F{7}$(git_current_branch)%f%b'
+RPROMPT='%B%F{7}$(git_current_branch)$(_git_worktree_indicator)%f%b'
 PROMPT_EOL_MARK='%B%F{7}↵%f%b'
