16b62f52309eb7bfc3c3f023d9ec127b1054153f
Jack Jennings <j@ckjennin.gs>
2026-07-27 14:16:07 -0700

    Revert "Switch gh account by directory, mirroring git identity includeIf"
    This reverts commit ce5c00dcbd950762550e01776a6896f43138f0a6.

--- home/dot_oh-my-zsh-custom/gh.zsh
+++ home/dot_oh-my-zsh-custom/gh.zsh
@@ -1,16 +1,1 @@
 export GH_NO_UPDATE_NOTIFIER=1
-
-gh() {
-  if [[ "$1" == "auth" ]]; then
-    command gh "$@"
-    return
-  fi
-
-  local account token
-  account="$(~/.config/git/bin/gh-account-for-pwd)"
-  token="$(command gh auth token --user "$account" 2>&1)" || {
-    echo "gh: failed to get token for account '$account': $token" >&2
-    return 1
-  }
-  GH_TOKEN="$token" command gh "$@"
-}
