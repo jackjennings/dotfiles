5900603ad35fda225904f6c6ae94879a34fc4560
Jack Jennings <j@ckjennin.gs>
2026-07-27 14:17:16 -0700

    Fix worktree detection in RPROMPT

--- home/dot_oh-my-zsh-custom/prompt.zsh
+++ home/dot_oh-my-zsh-custom/prompt.zsh
@@ -1,7 +1,10 @@
 setopt prompt_subst
 
 _git_worktree_indicator() {
-  [[ "$(git rev-parse --is-inside-work-tree 2>/dev/null)" == "true" ]] && echo ' (wt)'
+  local git_dir common_dir
+  git_dir="$(git rev-parse --git-dir 2>/dev/null)" || return
+  common_dir="$(git rev-parse --git-common-dir 2>/dev/null)" || return
+  [[ "$git_dir" != "$common_dir" ]] && echo ' (wt)'
 }
 
 PROMPT='%B%F{7}%1~%f%b ⎬ '
